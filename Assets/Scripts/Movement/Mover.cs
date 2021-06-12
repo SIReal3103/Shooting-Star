dc0792eafcc41cf9c7d20738d8fd7fe3924d829e
Kelen Nguyen <manhhieu012@gmail.com>
2021-06-12 09:36:23 +0700

    Movement

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class Mover : MonoBehaviour
+namespace Game.Movement
 {
-    public float speed;
-
-    Rigidbody2D rb;
-
-    Vector2 direction;
-
-    private void Start() {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void Update() {
-        rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
-        direction = Vector2.zero;
-    }
-
-    public void MoveTo(Vector2 direction)
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Mover : MonoBehaviour
     {
-        this.direction = direction.normalized;
+        public float speed = 10f;
+
+        Rigidbody2D rb;
+
+        Vector2 direction;
+
+        private void Start() {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update() {
+            rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+            direction = Vector2.zero;
+        }
+
+        public void MoveWith(Vector2 direction)
+        {
+            this.direction = direction.normalized;
+        }
     }
 }
