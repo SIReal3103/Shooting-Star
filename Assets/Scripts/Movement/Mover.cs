71b3fa95ccf73e2b4abdb72570faaf677d8de3eb
Kelen Nguyen <manhhieu012@gmail.com>
2021-06-12 08:53:31 +0700

    gitignore

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb;
+
+    Vector2 direction;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+        direction = Vector2.zero;
+    }
+
+    public void MoveTo(Vector2 direction)
+    {
+        this.direction = direction.normalized;
+    }
+}
