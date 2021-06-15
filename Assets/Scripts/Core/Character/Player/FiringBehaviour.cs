97ff240232815acaa06b563bd3ef1468025e3953
gameoflord012 <manhhieu012@gmail.com>
2021-06-15 10:55:47 +0700

    Add <Gun> Script - for change player gun

---
+++ Assets/Scripts/Core/Character/Player/FiringBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Core
+{
+    [RequireComponent(typeof(Character))]
+    public class FiringBehaviour : MonoBehaviour
+    {
+        /// <summary>
+        /// Add this class to player for auto firing.
+        ///
+        /// </summary>
+        /// 
+
+        
+
+        [SerializeField] float timeBetweenFire = 0.5f;
+        float timeSinceLastFire = Mathf.Infinity;
+
+        /// Player Gun
+        [Tooltip("Character Gun")]
+        public GameObject PlayerGun;
+
+        Gun _currentGun;
+        Character _character;
+
+        private void Start()
+        {
+            _character = GetComponent<Character>();
+
+            _currentGun = PlayerGun.GetComponent<Gun>();
+            ChangeGun(PlayerGun);
+        }
+
+        private void Update()
+        {
+            FireBehaviour();
+
+            UpdateTimer();
+        }
+
+        public void ChangeGun(GameObject gun)
+        {
+            if (_currentGun != null) Destroy(_currentGun.gameObject);
+
+            _currentGun = Instantiate(gun, transform.position, Quaternion.identity).GetComponent<Gun>();
+            _currentGun._character = _character;
+        }
+
+        private void UpdateTimer()
+        {
+            timeSinceLastFire += Time.deltaTime;
+        }
+
+        public void FireBehaviour()
+        {
+            if (timeSinceLastFire > timeBetweenFire)
+            {
+                _currentGun.Firing();
+
+                timeSinceLastFire = 0;
+            }
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Core/Gun/Gun.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Core
+{
+    public class Gun : MonoBehaviour
+    {
+        /// <summary>
+        /// Add this class to change gun by player.
+        /// </summary>
+
+        [Header("Gun")]
+        /// Number of bullet firing
+        public int NumberOfBullet;
+        /// Direction of each bullet;
+        [Tooltip("Direction of each bullet from left to right")]
+        public List<Vector2> BulletDirectionList = new List<Vector2>();
+        /// Bullet that gun firing;
+        public BulletPool BulletPrefab;
+        
+
+        [HideInInspector]
+        public Character _character;
+
+        public void Firing()
+        {
+            for (int i = 0; i < NumberOfBullet; i++)
+            {
+                BulletData bulletData = new BulletData(gameObject, _character.GetBulletSpawnPosition(), BulletDirectionList[i]);
+                BulletPrefab.Pop().InitData(bulletData);
+            }
+        }
+    }
+}
