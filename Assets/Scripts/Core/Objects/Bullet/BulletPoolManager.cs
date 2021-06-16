5212746502dfe3fffacc0c1ec04ff5f7dad16ba6
gameoflord012 <manhhieu012@gmail.com>
2021-06-16 21:03:07 +0700

    ExpandedDictionary Refactoring BulletPoolManager

--- Assets/Scripts/Core/Objects/Bullet/BulletPoolManager.cs
+++ Assets/Scripts/Core/Objects/Bullet/BulletPoolManager.cs
@@ -8,39 +8,23 @@ namespace Game.Core
     {
         protected BulletPoolManager() { }
 
-        public string DEFAULT_GUN_KEY = "";
+        [SerializeField] string DEFAULT_GUN_KEY = "BulletPoolLevel1";
 
-        private Dictionary<string, BulletPool> bulletPools = new Dictionary<string, BulletPool>();
+        ExpandedDictionary<BulletPool> bulletPools;
 
         private void Start()
         {
-            for(int i = 0; i < transform.childCount; i++)
-            {
-                Transform child = transform.GetChild(i);
-                bulletPools.Add(child.name, child.GetComponent<BulletPool>());
-
-                if (DEFAULT_GUN_KEY == "") DEFAULT_GUN_KEY = child.name;
-            }
+            bulletPools = new ExpandedDictionary<BulletPool>(gameObject);
         }
 
         public BulletPool GetBulletPool(string s)
         {
-            BulletPool pool;
-            if(bulletPools.TryGetValue(s, out pool))
-            {
-                throw new UnityException("Bullet pool key not found");
-            }
-            return pool;
+            return bulletPools.GetValueFrom(s);
         }
 
         public BulletPool GetBulletPool()
         {
-            BulletPool pool;
-            if(!bulletPools.TryGetValue(DEFAULT_GUN_KEY, out pool))
-            {
-                throw new UnityException("Default gun key is invalid");
-            }
-            return pool;
+            return bulletPools.GetValueFrom(DEFAULT_GUN_KEY);
         }
     }
 }
---
+++ Assets/Templates/Scripts/ExpandedDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandedDictionary<TObject>
+    where TObject : MonoBehaviour
+{
+    private Dictionary<string, TObject> dictionary;
+    GameObject container;
+
+    public ExpandedDictionary(GameObject container)
+    {
+        this.container = container;
+        dictionary = new Dictionary<string, TObject>();
+
+        LoadContainer();
+    }
+
+    void LoadContainer()
+    {
+        Transform transform = container.transform;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            TObject child = transform.GetChild(i).GetComponent<TObject>();
+
+            dictionary.Add(child.name, child);
+
+            Debug.Log(child.name);
+        }
+    }
+
+    public TObject GetValueFrom(string key)
+    {
+        TObject value;
+        if(dictionary.TryGetValue(key, out value))
+        {
+            throw new UnityException("No value in container for key: " + key);
+        }
+        return value;
+    }
+}
