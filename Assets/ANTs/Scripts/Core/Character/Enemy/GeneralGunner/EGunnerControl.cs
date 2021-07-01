b4f85b6f7c6a87e28f9ee14c2644d4e9e517d467
hiep <hiep.cbla5@gmail.com>
2021-07-01 09:53:02 +0700

    Add <EGunner (Task, BT, Control) - for gunner's enemy. (Not done)

---
+++ Assets/ANTs/Scripts/Core/Character/Enemy/GeneralGunner/EGunnerControl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace ANTs.Core
+{
+    [RequireComponent(typeof(EnemyControl))]
+    public class EGunnerControl : MonoBehaviour
+    {
+        public event Action OnActorArrivedEvent
+        {
+            add { GetComponent<MoveAction>().OnArrivedEvent += value; }
+            remove { GetComponent<MoveAction>().OnArrivedEvent -= value; }
+        }
+
+        public event Action OnActorAttackDone
+        {
+            add { GetComponent<AttackAction>().OnActionStopEvent += value; }
+            remove { GetComponent<AttackAction>().OnActionStopEvent -= value; }
+        }
+
+        [Tooltip("The speed which Charger normally move")]
+        [SerializeField] MoveData normalSpeed;
+        [Tooltip("The speed which charger run to target (or player)")]
+        [SerializeField] MoveData chargeSpeed;
+
+        private MoveAction move;
+        private AttackAction attack;
+
+        private void Awake()
+        {
+            move = GetComponent<MoveAction>();
+            attack = GetComponent<AttackAction>();
+        }
+
+        public void MoveTo(Vector2 destination)
+        {
+            move.SetMoveData(normalSpeed);
+            move.SetDestination(destination);
+        }
+
+        public void RunTo(Vector2 destination)
+        {
+            move.SetMoveData(chargeSpeed);
+            move.SetDestination(destination);
+        }
+
+        public void Attack()
+        {
+            attack.ActionStart();
+        }
+    }
+}
---
+++ Assets/ANTs/Scripts/Core/Character/Enemy/GeneralGunner/EGunnerTask.cs
@@ -0,0 +1,109 @@
+﻿using ANTs.Template;
+using Panda;
+using UnityEngine;
+
+namespace ANTs.Core
+{
+    public class EGunnerTask_01 : MonoBehaviour
+    {
+        [SerializeField] Transform player;
+        [SerializeField] ANTsPolygon prepareZone;
+        [SerializeField] AnimatorEvents animationEvent;
+
+        private EGunnerControl enemy;
+        private bool isArrived;
+        private bool isAttackDone;
+        private Vector2 preparePosition;
+
+        #region ======================================================Initialize
+        public void Awake()
+        {
+            enemy = GetComponent<EGunnerControl>();
+            preparePosition = prepareZone.GetRandomPointOnSurface();
+        }
+
+        public void OnEnable()
+        {
+            enemy.OnActorArrivedEvent += OnActorArrived;
+            enemy.OnActorAttackDone += OnActorAttackDone;
+        }
+
+        private void OnDisable()
+        {
+            enemy.OnActorArrivedEvent -= OnActorArrived;
+            enemy.OnActorAttackDone -= OnActorAttackDone;
+        }
+        #endregion
+
+
+
+
+        #region ==================================================Tasks
+        [Task]
+        public void ApproachPlayer()
+        {
+            if (Task.current.isStarting)
+            {
+                isArrived = false;
+                Vector2 playerPosition = player.position;
+                enemy.RunTo(playerPosition);
+            }
+
+            if (isArrived)
+            {
+                Task.current.Succeed();
+            }
+            Task.current.debugInfo = Task.current.status.ToString();
+        }
+
+        [Task]
+        public void MoveToPreparePosition()
+        {
+            if (Task.current.isStarting)
+            {
+                isArrived = false;
+                enemy.MoveTo(preparePosition);
+            }
+
+            if (isArrived) Task.current.Succeed();
+        }
+
+        [Task]
+        public void AttackPlayer()
+        {
+            if (Task.current.isStarting)
+            {
+                isAttackDone = false;
+                enemy.Attack();
+            }
+
+            if (isAttackDone) Task.current.Succeed();
+        }
+
+        [Task]
+        public void FirePlayer()
+        {
+            if (Task.current.isStarting)
+            {
+
+            }
+
+            Task.current.Succeed();
+        }
+        #endregion
+
+
+
+
+
+        public void OnActorArrived()
+        {
+            isArrived = true;
+        }
+
+        private void OnActorAttackDone()
+        {
+            isAttackDone = true;
+        }
+    }
+}
