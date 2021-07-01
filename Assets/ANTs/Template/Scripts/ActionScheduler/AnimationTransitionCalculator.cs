6c31d75dabbf5257713302cc42b37decd69aadd1
gameoflord012 <manhhieu012@gmail.com>
2021-07-01 19:19:00 +0700

    change anim

--- Assets/ANTs/Template/Scripts/ActionScheduler/AnimationTransitionCalculator.cs
+++ Assets/ANTs/Template/Scripts/ActionScheduler/AnimationTransitionCalculator.cs
@@ -7,8 +7,7 @@ namespace ANTs.Template
     {
         public event System.Action OnTransitionExitEvent;
 
-        const float ENTER_TIME_OFFSET = 0.1f;
-        const float TRANSITION_WAIT_TIME = 0.1f;
+        readonly private float ENTER_TIME_OFFSET = Time.maximumDeltaTime; // one frame
 
         private float transitionTimer;
         private bool isTransitionEntered;
@@ -22,27 +21,22 @@ namespace ANTs.Template
         }
 
         public IEnumerator FixedUpdate()
-        {
+        {            
             while (true)
             {
-                if (transitionTimer > TRANSITION_WAIT_TIME)
+                yield return new WaitForEndOfFrame();
+                if (isTransitionEntered)
                 {
-                    if (isTransitionEntered)
+                    if (AnimatorNormalizedTime() < ENTER_TIME_OFFSET)
                     {
-                        if (AnimatorNormalizedTime() < ENTER_TIME_OFFSET)
-                        {
-                            OnTransitionExitEvent?.Invoke();
-                            break;
-                        }
-                    }
-                    else if (AnimatorNormalizedTime() > ENTER_TIME_OFFSET)
-                    {
-                        isTransitionEntered = true;
+                        OnTransitionExitEvent?.Invoke();
+                        break;
                     }
                 }
-
-                transitionTimer += Time.deltaTime;
-                yield return new WaitForFixedUpdate();
+                else if (AnimatorNormalizedTime() > ENTER_TIME_OFFSET)
+                {
+                    isTransitionEntered = true;
+                }
             }
         }
 
