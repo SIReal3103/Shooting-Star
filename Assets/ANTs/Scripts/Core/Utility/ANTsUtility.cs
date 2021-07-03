418ae5f1107958a0473f99cc1d4487573ba9b280
hiep <hiep.cbla5@gmail.com>
2021-07-03 21:53:29 +0700

    pull

---
+++ Assets/ANTs/Scripts/Core/Utility/ANTsUtility.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ANTs
+{
+    public static class ANTsUtility
+    {
+        public static float ConvertVectorToAngle(Vector3 vector)
+        {
+            float angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+            return angle;
+        }
+    }
+}
