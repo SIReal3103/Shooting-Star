2ef999d5b090f7bda03c5841d8319184f4a34cb5
gameoflord012 <manhhieu012@gmail.com>
2021-07-13 10:48:47 +0700

    WIP

--- Assets/ANTs/Template/Scripts/Pool/ProgressablePoolManager.cs
+++ Assets/ANTs/Template/Scripts/Pool/ProgressablePoolManager.cs
@@ -6,14 +6,16 @@ namespace ANTs.Template
     public class ANTsPoolDecorator
     {
         public ANTsPool pool;
+        public string name;
         public ProgressIdentifier currentLevelId;
         public ProgressIdentifier nextLevelId;
 
-        public ANTsPoolDecorator(ANTsPool pool, ProgressIdentifier currentLevel, ProgressIdentifier nextLevel)
+        public ANTsPoolDecorator(ANTsPool pool, ProgressIdentifier currentLevel, ProgressIdentifier nextLevel, string name)
         {
             this.pool = pool;
             this.currentLevelId = currentLevel;
             this.nextLevelId = nextLevel;
+            this.name = name;
         }
     }
 
@@ -30,6 +32,9 @@ namespace ANTs.Template
         private Dictionary<ANTsPool, ANTsPoolDecorator> pool2Decorator =
             new Dictionary<ANTsPool, ANTsPoolDecorator>();
 
+        private Dictionary<string, ProgressIdentifier> name2Decorator =
+            new Dictionary<string, ProgressIdentifier>();
+
         protected ProgressablePoolManager() { }
 
         protected override void Awake()
@@ -38,7 +43,7 @@ namespace ANTs.Template
             foreach (TObject prefab in prefabs)
             {
                 ANTsPool pool = prefab.gameObject.GetOrCreatePool(transform);
-                ANTsPoolDecorator decorator = new ANTsPoolDecorator(pool, prefab.CurrentLevel, prefab.NextLevel);
+                ANTsPoolDecorator decorator = new ANTsPoolDecorator(pool, prefab.CurrentLevel, prefab.NextLevel, prefab.name);
                 pool2Decorator.Add(pool, decorator);
                 id2Decorator.Add(prefab.CurrentLevel, decorator);
             }
@@ -59,13 +64,18 @@ namespace ANTs.Template
             }
         }
 
-        public ANTsPool GetDefaultPool()
+        public ANTsPool GetPool(ProgressIdentifier id)
         {
-            if (id2Decorator.TryGetValue(defaultId, out ANTsPoolDecorator decorator))
+            if (id2Decorator.TryGetValue(id, out ANTsPoolDecorator decorator))
             {
                 return decorator.pool;
             }
-            throw new UnityException("Invalid default Id");
+            throw new UnityException("Invalid Id");
+        }
+
+        public ANTsPool GetDefaultPool()
+        {
+            return GetPool(defaultId);
         }
     }
 }
