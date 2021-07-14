412b6a19459afad8d846796abd1cd1b51b4b5ee5
hiep <hiep.cbla5@gmail.com>
2021-07-14 10:09:48 +0700

    Fix FPS
    add <fps counter> fix fps

---
+++ Assets/ANTs/Scripts/Test/HubAndLimitFPS.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubAndLimitFPS : MonoBehaviour
+{
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 60;
+    }
+}
